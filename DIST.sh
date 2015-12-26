d70a3968ea18d66bf2eca3d2966115c633cc424e
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:36:17 -0330

    add IoTQL

--- DIST.sh
+++ DIST.sh
@@ -35,12 +35,9 @@ echo "=================="
         package.json homestar.json \
         TCPConnectedBridge.js models/TCPConnectedLight.js tcp-connected.js \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
-
-    ## cp dist/*.* "${NPM_DST}" || exit 1
-
-    cd "${NPM_DST}" || exit 1
-    npm publish
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
 
     echo "end"
 )
--- models/TCPConnectedLight.js
+++ models/TCPConnectedLight.js
@@ -6,16 +6,7 @@
  *  2014-08-10
  */
 
-var iotdb = require("iotdb");
-
-exports.Model = iotdb.make_model('TCPConnectedLight')
-    .facet(":lighting")
-    .name("TCP Connected Light")
-    .io("on", iotdb.boolean.on)
-    .io("brightness", iotdb.number.percent.brightness)
-    .make();
-
 exports.binding = {
     bridge: require('../TCPConnectedBridge').Bridge,
-    model: exports.Model,
+    model: require('./TCPConnectedLight.json'),
 };
